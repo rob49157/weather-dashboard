0dd1c151de742ac917b4537134dd8e97419105cb
rob49157 <robertopaipo@hotmail.com>
2021-09-30 21:05:38 -0400

    variables

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 var searchitem =$("#formid")
 var searchbar= $("#city")
-var url= 
+var url= "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={913f8a0c9bf081d9e94bfd04b9efd30c}"
+var currenttemo= "&current.temp="
+var humidity= "&current.humidity"
+var currentweather= "&daily.weather.description="
+var uvindex= "&hourly.uvi="
+var wind= "&hourly.wind_speed"
+
+
 
 // click event
 $(searchitem).click(function(event){
