793cf33e66e16337ebbbc513a60140275856053b
roberto <robertopaipo@hotmail.com>
2021-10-02 12:16:15 -0400

    fetchcity

--- script.js
+++ script.js
@@ -1,62 +1,59 @@
-var searchitem =$("#formid")
-var searchbar= $("#city")
+var searchitem = $("#formid")
+var searchbar = $("#city")
+var searchcity = []
 //var url= "https://api.openweathermap.org/data/2.5/onecall?q=Atlanta&exclude=minutely&units=metric&current.temp=&daily.weather.description=&hourly.uvi=&hourly.wind_speed=&timezone=America/Atlanta&appid=913f8a0c9bf081d9e94bfd04b9efd30c"
-var currenturl="https://api.openweathermap.org/data/2.5/weather?q={searchcity}London&current.temp=&current.humidity=&daily.weather.description=&hourly.uvi=&hourly.wind_speed=&appid=913f8a0c9bf081d9e94bfd04b9efd30c"
-var searchcity= " "
-var currenttemp= "&current.temp="
-var humidity= "&current.humidity="
-var currentweather= "&daily.weather.description="
-var uvindex= "&hourly.uvi="
-var wind= "&hourly.wind_speed="
+var currenturl = ""
+
+var currenttemp = "&current.temp="
+var humidity = "&current.humidity="
+var currentweather = "&daily.weather.description="
+var uvindex = "&hourly.uvi="
+var wind = "&hourly.wind_speed="
 
 
 
 // click event
-$(searchitem).click(function(event){
+$(searchitem).click(function (event) {
     event.preventDefault()
     searchitem = searchbar.val()
-    searchcity = searchitem
-    console.log(searchitem)
+    currenturl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchitem + "&current.temp=&current.humidity=&daily.weather.description=&hourly.uvi=&hourly.wind_speed=&appid=913f8a0c9bf081d9e94bfd04b9efd30c"
+    // get api
+
+    fetch(currenturl)
+        .then(function (response) {
+            if (response.status !== 200) {
+                console.log("opps! something went wrong")
+            }
+            response.json()
+        })
+        .then(function (data) {
+            console.log(data)
+        })
 })
 
-
-// get api
-
-fetch(currenturl)
-.then(function(response){
-    if( response.status!== 200){
-    console.log( "opps! something went wrong")
-    }
-response.json()
-})
-.then(function(data){
-    appendData(data)
-    
-    // console.log(searchitem)
-})
 
 // create display for the fetch data
-function weatherdisplay(){
-    var div= document.getElementById("weatherdata")
-    for( i=0; i<data.lenght; i++){
-        var creatercontainer= document.createElement(div)
-        creatercontainer.innerHTML= data[i]
+function weatherdisplay() {
+    var div = document.getElementById("weatherdata")
+    for (i = 0; i < data.lenght; i++) {
+        var creatercontainer = document.createElement(div)
+        creatercontainer.innerHTML = data[i]
         div.appendChild(creatercontainer)
     }
 }
 
 
 
-    
+
 
 // local storage
-function save(cityname){
+function save(cityname) {
     localStorage.setItem(city.id, city.value)
 
 }
 
-for( var key in localStorage){
-    var citiesrememberd= $(key)
+for (var key in localStorage) {
+    var citiesrememberd = $(key)
     citiesrememberd.value = localStorage.getItem(key)
 }
 
