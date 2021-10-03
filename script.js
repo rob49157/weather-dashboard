580b09351f9d65f244892f9fa19daf9a37373db9
roberto <robertopaipo@hotmail.com>
2021-10-03 12:51:51 -0400

    displaying data

--- script.js
+++ script.js
@@ -4,11 +4,7 @@ var searchcity = []
 //var url= "https://api.openweathermap.org/data/2.5/onecall?q=Atlanta&exclude=minutely&units=metric&current.temp=&daily.weather.description=&hourly.uvi=&hourly.wind_speed=&timezone=America/Atlanta&appid=913f8a0c9bf081d9e94bfd04b9efd30c"
 var currenturl = ""
 
-var currenttemp = "&current.temp="
-var humidity = "&current.humidity="
-var currentweather = "&daily.weather.description="
-var uvindex = "&hourly.uvi="
-var wind = "&hourly.wind_speed="
+
 
 
 
@@ -16,7 +12,7 @@ var wind = "&hourly.wind_speed="
 $(searchitem).click(async function (event) {
     event.preventDefault()
     searchitem = searchbar.val()
-    currenturl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchitem + " ," + searchitem + " &current.temp=&current.humidity=&daily.weather.description=&hourly.uvi=&hourly.wind_speed=&appid=913f8a0c9bf081d9e94bfd04b9efd30c"
+    currenturl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchitem + " ," + searchitem + " &current.temp=&current.humidity=&daily.weather.description=&current.uvi=&hourly.wind_speed=&appid=913f8a0c9bf081d9e94bfd04b9efd30c"
     // get api
 
     data = await fetch(currenturl)
@@ -27,33 +23,25 @@ $(searchitem).click(async function (event) {
         return(data)
     })
     weatherdisplay(data)
-        // .then(function (response) {
-        //     if (response.status !== 200) {
-        //         console.log("opps! something went wrong")
-        //     }
-        //     response.json()
-        // })
-        // .then((data) => {
-        //     console.log(data)
-        // })
-        // .then(function (data) {
-        //     console.log(data)
-        // })
+      
 })
-
 
 // create display for the fetch data
   function weatherdisplay(data){
-      
-    var div = document.getElementById("weatherdata")
-    div.innerHTML = data.name
+      console.log(data)
+    var div1= document.getElementById("W1")  
+    var div2= document.getElementById("W2")
+    var div3= document.getElementById("W3")
+    var div4= document.getElementById("W4")
+    var div5= document.getElementById("W5")
+    div1.innerHTML= data.name
+    div2.innerHTML= data.main.temp
+    div3.innerHTML= data.main.temp_max
+    div4.innerHTML= data.main.temp_min
+    div5.innerHTML= data.wind.speed
     
-    }
-}
-
-
-
-
+  }
+    
 
 
 // local storage
